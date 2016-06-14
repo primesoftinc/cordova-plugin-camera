cea4354241afbebdfbcb00e049e29bf7068bebe5
Anurag Alla <dev.anurag.alla@gmail.com>
2016-06-14 11:25:18 +0530

    added resize param

--- www/Camera.js
+++ www/Camera.js
@@ -75,6 +75,7 @@ for (var key in Camera) {
  * @property {Boolean} [saveToPhotoAlbum] - Save the image to the photo album on the device after capture.
  * @property {module:CameraPopoverOptions} [popoverOptions] - iOS-only options that specify popover location in iPad.
  * @property {module:Camera.Direction} [cameraDirection=BACK] - Choose the camera to use (front- or back-facing).
+ * @property {number} [resizeFactor=80] - resizeFactor, expressed as a range of 0-100, where 100 is typically full resolution with no loss from file compression. (Note that information about the camera's resolution is unavailable.)
  */
 
 /**
@@ -154,9 +155,10 @@ cameraExport.getPicture = function(successCallback, errorCallback, options) {
     var saveToPhotoAlbum = !!options.saveToPhotoAlbum;
     var popoverOptions = getValue(options.popoverOptions, null);
     var cameraDirection = getValue(options.cameraDirection, Camera.Direction.BACK);
-
+    var resizeFactor = getValue(options.resizeFactor, 30); 
+    console.log("resizeFactor------------"+resizeFactor);
     var args = [quality, destinationType, sourceType, targetWidth, targetHeight, encodingType,
-                mediaType, allowEdit, correctOrientation, saveToPhotoAlbum, popoverOptions, cameraDirection];
+                mediaType, allowEdit, correctOrientation, saveToPhotoAlbum, popoverOptions, cameraDirection,resizeFactor];
 
     exec(successCallback, errorCallback, "Camera", "takePicture", args);
     // XXX: commented out
